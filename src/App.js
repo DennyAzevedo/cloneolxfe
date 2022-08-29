20f4fcdd81ed7d7471e7e2f329da3d0bbe7f2a65
DennyAzevedo <denny_af@yahoo.com.br>
2022-08-29 19:04:34 -0300

    Iniciando o projeto - ajustes do App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
 
-function App() {
+const Page = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Opa, funcinando...</div>
+  )
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Page)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import Reducers from './Reducers'
+
+const store = createStore(Reducers)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
