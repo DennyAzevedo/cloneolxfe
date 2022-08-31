1c5ac07e6683f4769f7262342f99582256701578
DennyAzevedo <denny_af@yahoo.com.br>
2022-08-31 19:52:54 -0300

    Ajustes iniciais do app.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import './App.css'
 import Routes from './Routes'
 import { Template } from './components/MainComponents'
 import Header from './components/partials/Header'
 import Footer from './components/partials/Footer'
+
 
 const Page = (props) => {
 	return (
