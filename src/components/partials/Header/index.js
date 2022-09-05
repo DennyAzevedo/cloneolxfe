1129030472bc508457b5647168f903a294794b28
DennyAzevedo <denny_af@yahoo.com.br>
2022-09-05 18:55:29 -0300

    Inciando Cabecalho - Logo

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { HeaderArea } from './styled'
 
 const Header = () => {
 	return (
 		<HeaderArea>
-			...
+			<div className="container">
+				<div className="logo">
+					<Link to="/">
+						<span className="logo-1">O</span>
+						<span className="logo-2">L</span>
+						<span className="logo-3">X</span>
+					</Link>
+				</div>
+			</div>
 		</HeaderArea>
 	)
 }
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,6 +1,34 @@
 import styled from 'styled-components'
 
 export const HeaderArea = styled.div`
-	height:100px;
-	background-color:#CCC;
+background-color: #FFF;
+height: 60px;
+border-bottom: 1px solid #CCC;
+.container {
+	max-width: 1000px;
+	margin: 0 auto;
+	display: flex;
+}
+
+a {
+	text-decoration: none;
+}
+
+.logo {
+	flex: 1;
+	display: flex;
+	align-items: center;
+	height: 60px;
+
+	.logo-1,
+	.logo-2,
+	.logo-3 {
+		font-size: 27px;
+		font-weight: bold;
+	}
+
+	.logo-1 { color: #F00; }
+	.logo-2 { color: #0F0; }
+	.logo-3 { color: #00F; }
+}
 `
