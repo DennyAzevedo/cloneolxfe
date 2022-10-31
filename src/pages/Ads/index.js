e8cdaae7d141426687da67bbc326050e30f8c090
DennyAzevedo <denny_af@yahoo.com.br>
2022-10-31 19:42:34 -0300

    Criando pagina para pesquisar anuncio - iniciando

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,16 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
-import { PageArea, SearchArea } from './styled';
-import { PageContainer, ErrorMessage } from '../../components/MainComponents';
+import
+React,
+{
+	useState,
+	useEffect
+} from 'react';
+import { useLocation, useHistory } from 'react-router-dom'
+import { PageArea } from './styled';
+import {
+	PageContainer,
+	ErrorMessage
+} from '../../components/MainComponents';
 import AdItem from '../../components/partials/AdItem'
 import useApi from '../../helpers/OlxAPI';
 
+let timer;
 const Page = () => {
 	const api = useApi();
+	const history = useHistory();
+
+	const useQueryString = () => {
+    return new URLSearchParams(useLocation().search);
+	}
+	const query = useQueryString();
+	const [q, setQ] = useState(query.get("q") != null ? query.get("q") : "");
+	const [cat, setCat] = useState(query.get("cat") != null ? query.get("cat") : "");
+	const [state, setState] = useState(query.get("state") != null ? query.get("state") : "");
 
 	const [stateList, setStateList] = useState([]);
 	const [categories, setCategories] = useState([]);
 	const [adList, setAdList] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1)
+  const [resultOpacity, setResultOpacity] = useState(1)
+  const [adsTotal, setAdsTotal] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  // const [warningMessage, setWarningMessage] = useState("Carregando...")
+  const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+    let queryString = []
+    if (q) {
+      queryString.push(`q=${q}`)
+    }
+    if (cat) {
+      queryString.push(`cat=${cat}`)
+    }
+    if (state) {
+      queryString.push(`state=${state}`)
+    }
+    history.replace({
+      search: `?${queryString.join('&')}`
+    })
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(getAdsList, 1000);
+    setResultOpacity(0.3);
+    setCurrentPage(1)
+  }, [q, cat, state]);
 
 	useEffect(() => {
 		const getStates = async () => {
@@ -28,72 +74,114 @@ const Page = () => {
 		getCategories();
 	}, []);
 
-	useEffect(() => {
-		const getRecentAds = async () => {
-			const json = await api.getAds({
-				sort: 'desc',
-				limit: 8
-			});
-			setAdList(json.ads);
-		}
-		getRecentAds();
-	}, []);
+	const getAdsList = async () => {
+    setLoading(true)
+    let offset = 0;
+    offset = (currentPage - 1) * 9
+    const Ads = await api.getAds({
+      sort: 'desc',
+      limit: 9,
+      q,
+      cat,
+      state,
+      offset
+    });
+    setAdList(Ads.ads)
+    setAdsTotal(Ads.total)
+    setResultOpacity(1);
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (adsTotal > 0) {
+      setPageCount(Math.ceil(adsTotal / adList.length))
+    } else {
+      setPageCount(0)
+    }
+  }, [adsTotal])
+
+  useEffect(() => {
+    setResultOpacity(0.3);
+    getAdsList()
+	}, [currentPage])
+	
+	let pagination = [];
+  for (let i = 0; i <= pageCount; i++) {
+    pagination.push(i + 1);
+  }
 
 	return (
-		<>
-			<SearchArea>
-				<PageContainer>
-					<div className="searchBox">
-						<form method="GET" action='/ads'>
-							<input
-								type="text"
-								name="q"
-								placeholder="O que você procura?"
-							/>
-							<select name="state">
-								{stateList.map((i,k) => 
-									<option key={k} value={i.name}>
-										{i.name}
-									</option>
-								)}
-							</select>
-							<button>Pesquisar</button>
-						</form>
-					</div>
-					<div className="categoryList">
-						{categories.map((i, k) => 
-							<Link
-								key={k}
-								to={`/ads?cat=${i.slug}`}
-								className="categoryItem"
+		<PageContainer>
+			<PageArea>
+				<div className="leftSide">
+          <form method="GET">
+						<input
+							type="text"
+							name="q"
+							placeholder="O que você procura?"
+              value={q}
+              onChange={(e) => setQ(e.target.value)}
+            />
+            <div className="filterName">Estado: </div>
+						<select
+							name="state"
+							value={state}
+							onChange={(e) => setState(e.target.value)}
+						>
+              <option></option>
+              {stateList.map((state, index) => (
+								<option
+									key={index}
+									value={state.id}
+								>
+									{state.name}
+								</option>
+              ))}
+            </select>
+            <div className="filterName">Categoria: </div>
+            <ul>
+              {categories.map((category, index) => (
+								<li
+									key={index}
+									className={cat === category.slug ? 'categoryItem active' : 'categoryItem'}
+                  onClick={() => setCat(category.slug)}
+                >
+                  < img src={category.img} alt="" />
+                  <span>{category.name}</span>
+                </li>
+              ))}
+            </ul>
+          </form>
+        </div>
+        <div className="rightSide">
+          <h2>Resultados</h2>
+          {loading && adList.length === 0 &&
+            <div className="ListWarning">Carregando ...</div>
+          }
+          {!loading && adList.length === 0 &&
+            <div className="ListWarning">Nenhum resultado encontrado</div>
+          }
+          <div className="list" style={{ opacity: resultOpacity }}>
+            {adList.map((ad, index) =>
+              <AddItem key={index} data={ad} />
+            )}
+          </div>
+          <div className="pagination">
+            {pagination.map((pg, index) => (
+							<div
+								onClick={() => setCurrentPage(pg)}
+								className={pg === currentPage ? 'pagItem active' : 'pagItem'}
+								key={index}
 							>
-								<img src={i.img} alt="" />
-								<span>{i.name}</span>
-							</Link>
-						)}
-					</div>
-				</PageContainer>
-			</SearchArea>
-			<PageContainer>
-				<PageArea>
-					<h2>Anúncios Recentes</h2>			
-					<div className="list">
-						{adList.map((i, k) => 
-							<AdItem key={k} data={i} />
-						)}
-					</div>
-					<Link to="/ads" className="seeAllLink">Ver todos</Link>
-					<hr />
-					O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.
-					O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde
-					o ano de 1500, quando uma misturou os caracteres de um texto para criar um
-					espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto
-					para a tipografia eletrônica, mantendo-se essencialmente inalterada.
-				</PageArea>			
-			</PageContainer>
-		</>
+                {pg}
+              </div>
+            ))}
+          </div>
+        </div>
+			</PageArea>
+		</PageContainer>
 		
-	)
+	);
 }
 
 export default Page;
--- src/pages/Ads/styled.js
+++ src/pages/Ads/styled.js
@@ -1,104 +1,107 @@
 import styled from 'styled-components';
 
-export const SearchArea = styled.div`
-	background-color: #DDD;
-	border-bottom: 1px solid #CCC;
-	padding: 20px 0;
-
-	.searchBox {
-		background-color: #9BB83C;
-		padding: 20px 15px;
-		border-radius: 5px;
-		box-shadow: 1px 1px 1px 0.3px rgba(0, 0, 0, 0.2);
-		display: flex;
-
-		form {
-			flex: 1%;
-			display: flex;
-
-			input, select {
-				height: 40px;
-				border: 0;
-				border-radius: 5px;
-				outline: 0;
-				font-size: 15px;
-				color: #000;
-				margin-right: 20px;
-			}
-
-			input {
-				flex: 1;
-				padding: 0 10px;
-			}
-
-			select {
-				width: 100px;
-			}
-
-			button {
-				background-color: #49AEEF;
-				font-size: 15px;
-				border: 0;
-				border-radius: 5px;
-				color: #FFF;
-				height: 40px;
-				padding: 0 20px;
-				cursor: pointer;
-
-				&:hover {
-					background-color: #429FDB;
-
-				}
-			}
-		}
-	}
-
-	.categoryList{
-		display: flex;
-		flex-wrap: wrap;
-		margin-top: 20px;
-
-		.categoryItem {
-			width: 25%;
-			display: flex;
-			align-items: center;
-			color: #000;
-			text-decoration: none;
-			height: 50px;
-			margin-bottom: 10px;
-
-			&:hover {
-				color: #999;
-			}
-
-			img {
-				width: 45px;
-				height: 45px;
-				margin-right: 10px;
-			}
-		}
-	}
-`;
-
 export const PageArea = styled.div`
-	h2 {
-		font-size: 20px;
-	}
+	display: flex;
+	margin-top: 20px;
 
-	.list {
-		display: flex;
-		flex-wrap: wrap;
+	.leftSide {
+		width: 250px;
+		margin-right: 10px;
 
-		.aditem {
-			width: 25%;
+		.filterName {
+			font-size: 15px;
+			margin: 10px 0;
 		}
-	}
 
-	.seeAllLink {
-		color: #000;
-		text-decoration: none;
-		font-weight: bold;
-		display: inline-block;
-		margin-top: 10px;
+    input, select{
+      width: 100%;
+      height: 40px;
+      border: 1px solid #9bb83c;
+      border-radius: 5px;
+      outline: none;
+			font-size: 15px;
+      color: #000;
+      padding: 10px;
+    }
+
+    ul, li{
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+
+    .categoryItem{
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      border-radius: 5px;
+      color: #000;
+      cursor: pointer;
+
+      img{
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+      }
+
+      span{
+        font-size: 14px;
+      }
+    }
+
+    .categoryItem:hover,
+    .categoryItem.active{
+      background-color: #9bb83c;
+      color: #fff;
+    }
+  }
+
+  .rightSide{
+    flex: 1;
+
+    h2{
+      margin-top:0;
+      font-size: 18px;
+    }
+		
+		.list{
+      display: flex;
+      flex-wrap: wrap;
+			
+      .aditem{
+        width:33%;
+      }
+    }
+
+    .ListWarning{
+      padding: 30px;
+      text-align: center;
+    }
+
+    .pagination{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 20px;
+      margin: 10px 0;
+      flex-wrap: wrap;
+      .pagItem{
+        width: 30px;
+        height: 30px;
+        display: flex;
+        border: 1px solid #000;
+        align-items: center;
+        justify-content: center;
+        font-size: 14px;
+        margin-right: 5px;
+        
+        &:hover{
+          border:1px solid #999;
+        }
+        &.active{
+          background-color:#CCC;
+        }
+      }
+    }
 	}
 `;
